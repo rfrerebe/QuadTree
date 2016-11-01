02f08529209fcb1ed5ea4175fbe86639a8935b1a
Rémy Frèrebeau <remy@frerebeau.org>
2016-11-01 23:59:58 +0100

    remove + findClosest (need to add some tests)

--- src/QuadTree.fs
+++ src/QuadTree.fs
@@ -1,8 +1,9 @@
 module QuadTree
 
-type Point = { Lat :float; Lng : float}
-type Bound = { Center : Point; HalfDimension :float }
+open System.Collections.Concurrent
 
+type Point = { Lat :float; Lng : float}
+type Bound = { Center : Point; HalfDimension : float }
 
 type Direction =
     | NW 
@@ -10,8 +11,8 @@ type Direction =
     | SW
     | SE
     
-type Node = { NW: QuadTree option ; NE: QuadTree option ; SE: QuadTree option; SW: QuadTree option }
-and QuadTree = { Bound : Bound; Max : int; mutable Index : int; mutable Data : Point array; mutable Node : Node option }
+type Node = { NW: QuadTree option; NE: QuadTree option; SE: QuadTree option; SW: QuadTree option }
+and QuadTree = { Bound : Bound; Max : int; Data : ConcurrentBag<Point>; mutable Node : Node option }
 
 let emptyNode = {NW = None; NE = None; SW = None; SE = None }
 
@@ -30,8 +31,7 @@ let intersect (b:Bound) (b': Bound) =
 let init p halfDimension max =
     { Bound = { Center = p; HalfDimension = halfDimension};
         Max = max;
-        Index = 0;
-        Data = Array.zeroCreate max;
+        Data = new ConcurrentBag<Point>();
         Node = None }
 
 let createSubQuadTree (q: QuadTree) (dir : Direction) =
@@ -101,21 +101,30 @@ let subdivide (q : QuadTree) (dir : Direction) =
             q.Node <- Some {emptyNode with SE = Some(nq) }
             nq
 
-let rec insert (p: Point) (q : QuadTree)  =
-    q |> ignore
+let getSubQuadTree (n: Node option) (d :Direction) : QuadTree option =
+    match n with
+    | None -> None
+    | Some n' ->
+        match d with
+        | NW -> n'.NW
+        | NE -> n'.NE
+        | SW -> n'.SW
+        | SE -> n'.SE
+
+
+let rec insert (p: Point)(q : QuadTree)  =
     match p |> contains q.Bound with
     | false -> false
     | true ->
-        match q.Index < q.Max with
+        match q.Data.Count < q.Max with
         | true -> 
-            q.Data.[q.Index] <- p
-            q.Index <- q.Index + 1
+            q.Data.Add p
             true
         | false ->
             match p.Lat, p.Lng with
             | x, y when x >= q.Bound.Center.Lat && y >= q.Bound.Center.Lng
                 -> subdivide q NW
-                    |> insert p 
+                    |> insert p
             | x, y when x >= q.Bound.Center.Lat && y < q.Bound.Center.Lng
                 -> subdivide q SW
                     |> insert p
@@ -125,3 +134,73 @@ let rec insert (p: Point) (q : QuadTree)  =
             | x, y -> subdivide q SE
                     |> insert p
 
+
+let rec remove (p : Point) (q: QuadTree) =
+    match p |> contains q.Bound with
+    | false -> false
+    | true ->
+        match q.Data.TryTake (ref p) with
+        | true -> true
+        | false ->
+            match p.Lat, p.Lng with
+            | x, y when x >= q.Bound.Center.Lat && y >= q.Bound.Center.Lng
+                ->  match getSubQuadTree q.Node NW with
+                    | None -> true // or should it be false ?
+                    | Some q ->  remove p q
+            | x, y when x >= q.Bound.Center.Lat && y < q.Bound.Center.Lng
+                ->  match getSubQuadTree q.Node SW with
+                    | None -> true // or should it be false ?
+                    | Some q ->  remove p q
+            | x, y when x < q.Bound.Center.Lat && y >= q.Bound.Center.Lng
+                ->  match getSubQuadTree q.Node NE with
+                    | None -> true // or should it be false ?
+                    | Some q ->  remove p q
+            | x, y 
+                ->  match getSubQuadTree q.Node SE with
+                    | None -> true // or should it be false ?
+                    | Some q ->  remove p q
+
+
+let findClosest (p: Point) (q: QuadTree) =
+    let distance p' =
+        (p'.Lat - p.Lat)**2.0 + (p'.Lng - p.Lng)**2.0
+    let minimum (m : Point option) ( m' : Point option) =
+        match m, m' with
+        | None, None -> None
+        | None, _ -> m'
+        | _, None -> m
+        | Some point, Some point' ->
+            seq [point; point']
+            |> Seq.minBy distance
+            |> Option.Some
+    let rec findClosest' (min : Point option)  (q' : QuadTree) =
+        match q.Data.IsEmpty with
+        | true -> min
+        | false ->
+            let min' =
+                q'.Data
+                |> Seq.minBy distance
+                |> Option.Some
+                |> minimum min
+            match p.Lat, p.Lng with
+            | x, y when x >= q'.Bound.Center.Lat && y >= q'.Bound.Center.Lng
+                ->  match getSubQuadTree q'.Node NW with
+                        | None -> min'
+                        | Some q -> findClosest' min' q
+            | x, y when x >= q'.Bound.Center.Lat && y < q'.Bound.Center.Lng
+                ->  match getSubQuadTree q'.Node SW with
+                    | None -> min'
+                    | Some q -> findClosest' min' q
+            | x, y when x < q'.Bound.Center.Lat && y >= q'.Bound.Center.Lng
+                ->  match getSubQuadTree q'.Node NE with
+                    | None -> min'
+                    | Some q -> findClosest' min' q
+            | x, y 
+                ->  match getSubQuadTree q'.Node SE with
+                    | None -> min'
+                    | Some q -> findClosest' min' q
+
+    match p |> contains q.Bound with
+    | false -> None
+    | true -> findClosest' None q
+    

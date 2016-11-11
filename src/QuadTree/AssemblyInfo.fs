8cfa8c90cf3da5c8966383339199111f6f0441c4
Rémy Frèrebeau <remy@frerebeau.org>
2016-11-11 22:31:28 +0100

    package for nuget

--- src/QuadTree/AssemblyInfo.fs
+++ src/QuadTree/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("QuadTree")>]
 [<assembly: AssemblyProductAttribute("QuadTree")>]
 [<assembly: AssemblyDescriptionAttribute("Provide a QuadTree structure")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "QuadTree"
     let [<Literal>] AssemblyProduct = "QuadTree"
     let [<Literal>] AssemblyDescription = "Provide a QuadTree structure"
-    let [<Literal>] AssemblyVersion = "1.0"
-    let [<Literal>] AssemblyFileVersion = "1.0"
+    let [<Literal>] AssemblyVersion = "0.1"
+    let [<Literal>] AssemblyFileVersion = "0.1"
